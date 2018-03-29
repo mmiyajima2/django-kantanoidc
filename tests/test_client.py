04c863164843321145db7cbf3651a5bee030303e
MIYAJIMA,Masafumi <mmiyajima2@gmail.com>
2018-03-29 09:06:51 +0000

    Mod

--- tests/test_client.py
+++ tests/test_client.py
@@ -17,6 +17,9 @@ class KaocClientTests(TestCase):
         redirect_uri = 'https://watashi.me.local/callback'
         client = KaocClient(redirect_uri)
         self.assertEquals(redirect_uri, client.redirect_uri)
+        self.assertEquals('id', client.client_id)
+        self.assertEquals('secret', client.client_secret)
+
         self.assertIsNotNone(client.authorization_endpoint)
         logger.debug(client.authorization_endpoint)
         self.assertIsNotNone(client.token_endpoint)
