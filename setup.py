4bd7e825a26d60d02e8b75f37571e506f5b64923
MIYAJIMA,Masafumi <mmiyajima2@gmail.com>
2019-02-07 11:41:13 +0000

    Fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 
 def read(fname):
-    return open(os.path.jo(os.path.dirname(__file__), fname)).read()
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 setup(
